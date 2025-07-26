e1c173d9db644aed33f06c5e57f5e1383e4d807d
traolonggg <220100216@st.phenikaa-uni.edu.vn>
2025-07-26 21:55:36 +0700

    lam lai tu dau

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import pygame
-from constants import * 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    while True :
-             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    return
-             screen.fill("black")
-             pygame.display.flip()
+from constants import *
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+def main() :
+
+
+    while True : 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
 if __name__ == "__main__":
     main()
+
