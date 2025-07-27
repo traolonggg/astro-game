59a246b269cc1231feec8cccad890bcc47ac0b17
traolonggg <220100216@st.phenikaa-uni.edu.vn>
2025-07-27 08:50:57 +0700

    update spaceship

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import pygame
 from constants import *
+from player import Player
+pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+dt = 0
+player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
+
 def main() :
 
 
@@ -8,9 +14,11 @@ def main() :
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60)/1000
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
-
 if __name__ == "__main__":
     main()
 
